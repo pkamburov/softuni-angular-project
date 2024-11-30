9b30427440db2d061271104e95412dbccb03fc90
Pavel Kamburov <pkamburov89@gmail.com>
2024-11-30 13:27:41 +0200

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyAMx7VGpgIEavaZvr7xZsjcA4LBuB7DfxE",
+        authDomain: "softuni-angular-exam-project.firebaseapp.com",
+        projectId: "softuni-angular-exam-project",
+        storageBucket: "softuni-angular-exam-project.firebasestorage.app",
+        messagingSenderId: "502288727002",
+        appId: "1:502288727002:web:55b74ed5a8e6306a095ee3"
+      }
+};
