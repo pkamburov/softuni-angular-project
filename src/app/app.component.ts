a7e11e46f74a01cf2c089be17654bd36c1748dff
Pavel Kamburov <pkamburov89@gmail.com>
2024-11-30 15:38:08 +0200

    CSS fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./core/header/header.component";
 import { FooterComponent } from './core/footer/footer.component';
 import { MainComponent } from "./main/main.component";
+import { LeftSidebarComponent } from "./left-sidebar/left-sidebar.component";
+import { RightSidebarComponent } from "./right-sidebar/right-sidebar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MainComponent, LeftSidebarComponent, RightSidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
