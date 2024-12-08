b4ec4b323fc452f33d84493c54c8d4b4ffab8221
Pavel Kamburov <pkamburov89@gmail.com>
2024-12-08 14:24:21 +0200

    Initial commit

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn } from "@angular/router";
+import { UserService } from "../user/user.service";
+import { inject } from "@angular/core";
+
+
+export const AuthGuard: CanActivateFn = () => {
+    const userService = inject(UserService);
+    return userService.isLoggedIn;
+}
