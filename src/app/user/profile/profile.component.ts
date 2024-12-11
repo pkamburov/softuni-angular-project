3ed1c7c35d9c7a9a5a012577bd4a72f099eb49bd
Pavel Kamburov <pkamburov89@gmail.com>
2024-12-11 11:15:35 +0200

    Profile.component initial commit

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,20 +1,88 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { UserService } from '../user.service';
+import { ApiService } from '../../api.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { Post } from '../../types/post';
+import { User, UserForAuth } from '../../types/user';
+import { PostSingleComponent } from '../../post/post-single/post-single.component';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../../shared/loader/loader.component';
+import { PostService } from '../../post/post.service';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    MatTabsModule,
+    MatButtonModule,
+    MatIconModule,
+    PostSingleComponent,
+    CommonModule,
+    LoaderComponent
+  ],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent implements OnInit {
+  // @ViewChild('postContainer') postContainer!: ElementRef;
+  posts: Post[] = [];
+  user: UserForAuth | null = null;
+  userId: string | null = null;
+  currentPage = 1;
+  limit = 50;
+  isLoading = true;
 
   constructor(
-    private route: ActivatedRoute,
+    private userService: UserService,
+    private apiService: ApiService,
+    private postService: PostService,
   ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.params['userId'];
-    
+    this.userService.getUserProfile().subscribe({
+      next: (user) => {
+        if (user) {
+          this.user = user;
+          this.userId = user._id;
+          this.loadUserPosts();
+        }
+      },
+    });
+    this.isLoading = false;
+  }
+
+  loadUserPosts() {
+    this.apiService
+      .getPosts(this.currentPage, this.limit)
+      .subscribe((posts) => {
+        const updatedPosts = posts.filter((post) => post.userId._id === this.userId);
+        if (updatedPosts) {
+          this.posts = [...this.posts, ...updatedPosts];
+        } else {
+          this.posts = posts;
+        }
+      });
+      this.isLoading = false;
+  }
+
+  get userPosts() {
+    return this.posts.length;
+  }
+
+  loadUserLikedPosts() {
+
+  }
+
+  onDeletePost(postId: string): void {
+    this.postService.deletePost(postId).subscribe({
+      next: () => {
+        this.posts = this.posts.filter(post => post._id !== postId);
+        console.log('deleted');
+      },
+      error: err => {
+        console.log('Error', err);
+      }
+    })
   }
 }
